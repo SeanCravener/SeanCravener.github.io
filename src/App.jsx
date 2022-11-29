9b63683ef746a532f80105f37d30e9fa520bfee8
Fatgotenkz <99251238+Fatgotenkz@users.noreply.github.com>
2022-11-28 23:50:07 -0500

    Working on Portfolio Theme
    Started setting up files to start designing the front end for my portfolio using React. Got vite set up and installed the react95 theme.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button, ThemeProvider } from '@react95/core';
+import '@react95/icons/icons.css';
+
+const MyApp = () => (
+  <ThemeProvider>
+    <Button>Click me!</Button>
+  </ThemeProvider>
+);
+
+export default MyApp
