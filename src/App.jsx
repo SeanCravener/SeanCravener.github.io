4bbb8c9a13077b149f61e657c6f75ac9005b5443
Fatgotenkz <99251238+Fatgotenkz@users.noreply.github.com>
2022-12-07 21:39:54 -0500

    tests
    Just performing some tests with react95

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
-import { Button, ThemeProvider } from '@react95/core';
+import React, { useState } from "react";
+import { Modal, TaskBar, List } from '@react95/core';
+import { ReaderClosed, WindowsExplorer } from '@react95/icons'
 import '@react95/icons/icons.css';
 
-const MyApp = () => (
-  <ThemeProvider>
-    <Button>Click me!</Button>
-  </ThemeProvider>
-);
+const App = () => {
+  const [first, toggleFirst] = useState(false);
+  const [second, toggleSecond] = useState(false);
 
-export default MyApp
+  const closeFirst = () => toggleFirst(false);
+  const closeSecond = () => toggleSecond(false);
+
+  return (
+    <>
+      {first && (
+        <Modal
+          icon={<WindowsExplorer variant="16x16_4" />}
+          title="Windows Explorer"
+          closeModal={closeFirst}
+          width="300"
+          height="200"
+        />
+      )}
+
+      {second && (
+        <Modal
+          defaultPosition={{ x: 50, y: 50 }}
+          width="300"
+          height="200"
+          icon={<ReaderClosed variant="16x16_4" />}
+          title="Local Disk (C:)"
+          closeModal={closeSecond}
+        />
+      )}
+
+      <TaskBar
+        list={
+          <List>
+            <List.Item
+              icon={<ReaderClosed variant="32x32_4" />}
+              onClick={() => toggleSecond(true)}
+            >
+              Local Disk (C:)
+            </List.Item>
+            <List.Item
+              icon={<WindowsExplorer variant="32x32_4" />}
+              onClick={() => {
+                toggleFirst(true);
+              }}
+            >
+              Windows Explorer
+            </List.Item>
+          </List>
+        }
+      />
+    </>
+  );
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MyApp from './App'
-import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyApp />
+    <App />
   </React.StrictMode>
 )
